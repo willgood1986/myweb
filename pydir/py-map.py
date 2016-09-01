b07f49a46c22f043e5c6976360bf4743db7d18c8
rainy <willgood1986@outlook.com>
2016-09-01 08:07:47 -0700

    Succeeds to execute the py-map

--- pydir/py-map.py
+++ pydir/py-map.py
@@ -1,7 +1,14 @@
+print('''
+This program introduces a function list(proc, existinglist)
+''')
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 def normalize(name):
     return name.capitalize();
 
 words = ['abc', 'Kitty', 'really']
-words = list(map(normalize, words))
-print words
+newwords = list(map(normalize, words))
+print('Old words:')
+print(words)
+print('New words:')
+print(newwords)
