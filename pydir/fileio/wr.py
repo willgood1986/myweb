e8a02f1978f0addc6e062962c2ac82dd53c76bf2
willgood1986 <willgood1986@outlook.com>
2016-09-17 00:17:35 -0700

    add file io demo

---
+++ pydir/fileio/wr.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+class myfile(object):
+    def __init__(self, name):
+        self._name = name
+        init(self._name)
+
+    @property
+    def Name(self):
+        return self._name
+    
+    def init(self, name)
+        pass
+
+
+if __name__ == '__main__':
+    with file.open('test.txt', 'w', encoding:)
+    
+    
